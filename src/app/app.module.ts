2bfb7ecda19692df9cac1bf2c7707265de560ea6
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-05-08 20:02:33 -0400

    cover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CoversComponent } from './covers/covers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoversComponent
   ],
   imports: [
     BrowserModule,
