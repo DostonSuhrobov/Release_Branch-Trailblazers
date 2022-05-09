9cd6d88bebfe9fd105b4efbc7495913b355d190f
Doston <36619036+DostonSuhrobov@users.noreply.github.com>
2022-05-09 19:04:49 -0400

    Delete login directory

--- login/login.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector:"login-modal",
-  templateUrl: "./login.component.html",
-  styleUrls:["./login.component.css"]
-})
-
-export class LoginComponent {}
