3297c86d294c01a97b86e4ad70161febf5cc3e94
Doston <36619036+DostonSuhrobov@users.noreply.github.com>
2022-05-15 20:32:14 -0400

    TMDB API keys
    full api keys added as comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Release_Branch-Trailblazers';
 }
+// API Key (v3 auth) : d89a01f68d806160a716281e3336d0a7
+// Example API Request : https://api.themoviedb.org/3/movie/550?api_key=d89a01f68d806160a716281e3336d0a7
+// API Read Access Token (v4 auth) : eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODlhMDFmNjhkODA2MTYwYTcxNjI4MWUzMzM2ZDBhNyIsInN1YiI6IjYyODE5OGE3NzUxMTBkMDA5ZmRlZWQ0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5aEEQ8VU0FKibQVAi7RxAQQ3oiJRvVub_EUXPrhZwZM
