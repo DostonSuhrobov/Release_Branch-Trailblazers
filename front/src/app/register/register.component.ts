eecd1b98f99deb61b3598bf7d34730f71fd1baff
RONGNA HUANG <wbb520wing@gmail.com>
2022-06-01 13:43:58 -0700

    sign up backend

--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BackendApiService } from '../services/backend-api.service';
 
 @Component({
   selector: 'app-register',
@@ -10,36 +12,43 @@ export class RegisterComponent implements OnInit {
 
   myForm!: FormGroup;
 
-  get password() {
-    return this.myForm.get('password')
-  }
-  get name() {
-    return this.myForm.get('name')
-  }
+  congratIcon = '&#127881;'
 
-  get email() {
-    return this.myForm.get('email')
-  }
+constructor(private formbulider: FormBuilder, private backendApiService: BackendApiService, private route: Router) {}
 
-  get gender() {
-    return this.myForm.get('gender')
-  }
 
-constructor(private formbulider: FormBuilder) {
-
+get password() {
+  return this.myForm.get('password')
 }
+get username() {
+  return this.myForm.get('username')
+}
+
+get email() {
+  return this.myForm.get('email')
+}
+
 
 ngOnInit(): void {
   this.myForm = this.formbulider.group({
-    password: ['', Validators.required, Validators.minLength(6)],
-    name: ['',Validators.required],
-    email: ['', Validators.required, Validators.email],
-    gender: ['', Validators.required]
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    username: ['', [Validators.required, Validators.maxLength(10)]],
+    email: ['', [Validators.required, Validators.email]],
   })
 }
 
-onSubmit() {
+onSubmit(f: FormGroup) {
 
+      const email = f.value.email;
+      const password = f.value.password;
+      const username = f.value.username
+
+      this.backendApiService.signUp(email,password,username).subscribe()
+      this.myForm.reset();
+}
+
+switchToLogin(){
+  this.route.navigate(['/login'])
 }
 
 }
